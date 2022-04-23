6f95cd7a2938773cfa970faee5e6e98f36a1cc5d
YounisCodeer <80555260+YounisCodeer@users.noreply.github.com>
2022-04-23 09:44:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Math.floor(); {
+    random_no = Math.floor((Math.random()*array_1.length)+1);
+}
+
+console.log(); {
+    console.log("Random Number")
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+timer_counter = 0
+timer_checker = ""
+Drawn_sketch = ""
+answer_holder = ""
+score = 0
